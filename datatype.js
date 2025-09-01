276a2c2119c80837da6f6d47cd0c0aa404f069dc
Skanda M V <skandagowda0@gmail.com>
2025-09-01 18:08:01 +0530

    Data Type

---
+++ datatype.js
@@ -0,0 +1,17 @@
+// Data Type in JavaScript 
+
+// Having Two Types of Data Type
+
+//   Primitive Data Type
+//     * It Stored directly in the location that the variable accesses.
+//     * It Strore in the Stack Memory.
+//     * It is not an object and has no methods.
+//     * It is immutable (cannot be altered).
+//     * Examples: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+
+//  Reference Data Type
+//     * It Stored in the Heap Memory and the variable contains a reference to that location.
+//     * It is an object and has methods.
+//     * It is mutable (can be altered).
+//     * Examples: Object, Array, Function, Date, RegExp, etc.
